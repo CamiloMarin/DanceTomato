0bd4227e00fa8a7e816a07724cc1356931f823f7
Miloka <camilomv13@hotmail.com>
2021-03-03 09:13:36 -0500

    .
    Arreglitos

--- Proyecto_Unity_Vectores_A.R_Dance/Assets/Script/Bullet.cs
+++ Proyecto_Unity_Vectores_A.R_Dance/Assets/Script/Bullet.cs
@@ -6,8 +6,11 @@ public class Bullet : MonoBehaviour
 {
     public GameObject bullet;
     public float speed = 100.0f;
-    public float timer = 2.0f;
+    private float timer = 2.0f;
     GameObject bullet_Clone;
+    public float SpawnTimeHigh = 10f;
+    public float SpawnTimeLow = 5f;
+
     void Start()
     {
         
@@ -23,7 +26,7 @@ public class Bullet : MonoBehaviour
             bullet_Clone.transform.localRotation = Quaternion.Euler(new Vector3(Random.Range(0f, 360f), 0, Random.Range(0f, 360f)));
             Rigidbody inst_Bullet_RB = bullet_Clone.GetComponent<Rigidbody>();
             inst_Bullet_RB.AddForce(Vector3.forward * speed);
-            timer = 2;
+            timer = Random.Range(SpawnTimeLow, SpawnTimeHigh);
         }
         
     }
